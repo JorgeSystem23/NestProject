2b15fc497dba425749bcd7b1f11bc8e3a62fca01
JorgeSystem23 <jgrullon@popoyan.com>
2024-04-11 14:20:31 -0600

    Section 4 completed of the NestJS course - Devtailles

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -5,10 +5,12 @@ import {
   Get,
   Param,
   ParseIntPipe,
+  ParseUUIDPipe,
   Patch,
   Post,
 } from "@nestjs/common";
 import { CarsService } from "./cars.service";
+import { CreateCarDto } from "./dtos/create-car.dto";
 
 @Controller("cars")
 export class CarsController {
@@ -20,14 +22,14 @@ export class CarsController {
   }
 
   @Get(":id")
-  getCarById(@Param("id", ParseIntPipe) id: number) {
+  getCarById(@Param("id", ParseUUIDPipe) id: string) {
     console.log({ id });
     return this.carsService.findOneById(id);
   }
 
   @Post()
-  createCar(@Body() body: any) {
-    return body;
+  createCar(@Body() createCarDto: CreateCarDto) {
+    return createCarDto;
   }
 
   @Patch(":id")
---
+++ src/cars/dtos/create-car.dto.ts
@@ -0,0 +1,5 @@
+// Los dto siempre son classes
+export class CreateCarDto {
+  readonly brand: string;
+  readonly model: string;
+}
